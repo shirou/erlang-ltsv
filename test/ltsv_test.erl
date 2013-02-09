fc1fb7a6f17fea989fd0658bdf0553a13532898d
shirou <shirou.faw@gmail.com>
2013-02-09 22:17:47 +0900

    add parse_file test.

--- test/ltsv_test.erl
+++ test/ltsv_test.erl
@@ -11,3 +11,14 @@ parse_line_test() ->
 	%% with newline
 	?assert(ltsv:parse_line("a:b\tc:d\n") =:=
 				[{<<"a">>,<<"b">>},{<<"c">>,<<"d">>}]).
+
+parse_file_test() ->
+	T = ltsv:parse_file("../test/test.tsv"),
+	?assert(lists:nth(1, T) =:=
+				[{<<"1">>,<<"a">>},{<<"2">>,<<"bb">>},{<<"3">>,<<"d:e">>}]),
+	?assert(lists:nth(2, T) =:=
+				[{<<"11">>,<<"a">>},{<<"12">>,<<"bb">>},{<<"13">>,<<"d:e">>}]),
+	?assert(lists:nth(3, T) =:=
+				[{<<"21">>,<<"a">>},{<<"22">>,<<"bb">>},{<<"33">>,<<227,131,134,227,130,185,227,131,136>>}]),
+	?assert(lists:nth(4, T) =:=
+				[{<<"21">>,<<"double">>},{<<"22">>,<<"bb">>},{<<"21">>,<<"double2">>}]).
