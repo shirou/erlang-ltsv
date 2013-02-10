e706424a1f4b33c96a43e362c0d3bed112d5fe5f
shirou <shirou.faw@gmail.com>
2013-02-10 23:46:47 +0900

    change rebar.config to fail_on_warning and so on.
    Since add warn_unused_import, import in the test is also changed.

--- test/ltsv_tests.erl
+++ test/ltsv_tests.erl
@@ -1,29 +1,31 @@
 -module(ltsv_tests).
--import(ltsv, [parse_line/1, parse_file/1, write/1]).
+-import(ltsv, [parse/1, parse_line/1, parse_file/1,
+			   to_binary/1, to_list/1,
+			   get_fields/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 
 parse_test() ->
-	?assertEqual([[{<<"a">>,<<"b">>}]], ltsv:parse("a:b")),
-	?assertEqual([[{<<"a">>,<<"b">>}, {<<"1">>,<<"2">>}]], ltsv:parse("a:b\t1:2")),
+	?assertEqual([[{<<"a">>,<<"b">>}]], parse("a:b")),
+	?assertEqual([[{<<"a">>,<<"b">>}, {<<"1">>,<<"2">>}]], parse("a:b\t1:2")),
 	?assertEqual([[{<<"a">>,<<"b">>}, {<<"1">>,<<"2">>}],
 				  [{<<"a">>,<<"b">>}, {<<"1">>,<<"2">>}]],
-				 ltsv:parse("a:b\t1:2\na:b\t1:2")),
+				 parse("a:b\t1:2\na:b\t1:2")),
 	%% check binary
 	?assertEqual([[{<<"a">>,<<"b">>}, {<<"1">>,<<"2">>}],
 				  [{<<"a">>,<<"b">>}, {<<"1">>,<<"2">>}]],
-				 ltsv:parse(<<"a:b\t1:2\na:b\t1:2">>)).
+				 parse(<<"a:b\t1:2\na:b\t1:2">>)).
 
 parse_line_test() ->
 	%% one tuple
-	?assertEqual([{<<"a">>,<<"b">>}], ltsv:parse_line("a:b")),
+	?assertEqual([{<<"a">>,<<"b">>}], parse_line("a:b")),
 	%% with tab
-	?assertEqual([{<<"a">>,<<"b">>},{<<"c">>,<<"d">>}], ltsv:parse_line("a:b\tc:d")),
+	?assertEqual([{<<"a">>,<<"b">>},{<<"c">>,<<"d">>}], parse_line("a:b\tc:d")),
 	%% with newline
-	?assertEqual([{<<"a">>,<<"b">>},{<<"c">>,<<"d">>}], ltsv:parse_line("a:b\tc:d\n")).
+	?assertEqual([{<<"a">>,<<"b">>},{<<"c">>,<<"d">>}], parse_line("a:b\tc:d\n")).
 
 parse_file_test() ->
-	T = ltsv:parse_file("../test/test.tsv"),
+	T = parse_file("../test/test.tsv"),
 	?assertEqual([{<<"1">>,<<"a">>},{<<"2">>,<<"bb">>},{<<"3">>,<<"d:e">>}],
 				 lists:nth(1, T)),
 	?assertEqual([{<<"11">>,<<"a">>},{<<"12">>,<<"bb">>},{<<"13">>,<<"d:e">>}],
@@ -37,16 +39,16 @@ to_binary_test() ->
 	Src = [[{<<"1">>,<<"a">>},{<<"2">>,<<"bb">>},{<<"3">>,<<"d:e">>}],
 		   [{<<"1">>,<<"a">>},{<<"2">>,<<"bb">>},{<<"3">>,<<"d:e">>}]],
 	Expected = <<"1:a\t2:bb\t3:d:e1:a\t2:bb\t3:d:e">>,
-	?assertEqual(Expected, ltsv:to_binary(Src)).
+	?assertEqual(Expected, to_binary(Src)).
 
 to_list_test() ->
 	Src = [[{<<"1">>,<<"a">>},{<<"2">>,<<"bb">>},{<<"3">>,<<"d:e">>}],
 		   [{<<"1">>,<<"a">>},{<<"2">>,<<"bb">>},{<<"3">>,<<"d:e">>}]],
 	Expected = [<<"1:a\t2:bb\t3:d:e">>, <<"1:a\t2:bb\t3:d:e">>],
-	?assertEqual(Expected, ltsv:to_list(Src)).
+	?assertEqual(Expected, to_list(Src)).
 
 
 get_fields_test() ->
 	Src = "1:a\t2:bb\t3:d:e\n1:a\t2:bb\t3:d:e",
-	S = ltsv:parse(Src),
-	?assertEqual([<<"bb">>, <<"bb">>], ltsv:get_fields(S, <<"2">>)).
+	S = parse(Src),
+	?assertEqual([<<"bb">>, <<"bb">>], get_fields(S, <<"2">>)).
